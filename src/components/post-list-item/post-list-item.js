9b0698d942b502754b2ba7e9863d267fe3a19424
shobilo <58330458+shobilo@users.noreply.github.com>
2021-07-24 15:52:49 +0300

    fav and important notes functionality

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,31 +1,59 @@
 import "./post-list-item.css"
+import {Component} from "react"
 
-const PostListItem = ({label, important = false}) => {
+export default class PostListItem extends Component {
 
-  let classNames = 'app-list-item d-flex justify-content-between'
-
-  if (important) {
-    classNames += ' important'
+  state = {
+    important: false,
+    like: false
   }
 
-  return (
-    <div className={classNames}>
-      <span className="app-list-item-label">
-        {label}
-      </span>
-      <div className="d-flex justify-content-center align-items-center">
-        <button className="btn-star btn-sm" 
-                type="button">
-          <i className="fa fa-star"></i>
-        </button>
-        <button className="btn-trash btn-sm" 
-                type="button">
-          <i className="fa fa-trash-o"></i>
-        </button>
-        <i className="fa fa-heart"></i>
-      </div>
-    </div>
-  )
-}
+  onImportant = () => {
+    this.setState(state => ({
+      important: !state.important
+    }))
+  }
 
-export default PostListItem
+  onLike = () => {
+    this.setState(state => ({
+      like: !state.like
+    }))
+  }
+
+  render() {
+    const {label} = this.props
+    const {important, like} = this.state
+
+    let classNames = 'app-list-item d-flex justify-content-between'
+
+    if (important) {
+      classNames += ' important'
+    }
+
+    if (like) {
+      classNames += ' like'
+    }
+
+    return (
+      <div className={classNames}>
+        <span 
+          className="app-list-item-label"
+          onClick={this.onLike}>
+            {label}
+        </span>
+        <div className="d-flex justify-content-center align-items-center">
+          <button className="btn-star btn-sm" 
+                  type="button"
+                  onClick={this.onImportant}>
+            <i className="fa fa-star"></i>
+          </button>
+          <button className="btn-trash btn-sm" 
+                  type="button">
+            <i className="fa fa-trash-o"></i>
+          </button>
+          <i className="fa fa-heart"></i>
+        </div>
+      </div>
+    )
+  }
+}
