39ae03ab0e6360a9cc00557c56aa9b21add5898d
shobilo <58330458+shobilo@users.noreply.github.com>
2021-07-26 13:18:27 +0300

    Add note functionality
    now to add note you should input text in input field and press Add button

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,21 +1,50 @@
+import { Component } from 'react'
+
 import "./post-add-form.css"
 
-const PostAddForm = ({onAdd}) => {
-  return (
-    <form className="bottom-panel d-flex">
-      <input 
-        type="text"
-        placeholder="Input your note here"
-        className="form-control new-post-label"
-      />
-      <button type="submit"
-              className="btn btn-outline-secondary"
-              onClick={() => onAdd('Hello')}
-              >
-        Add note
-      </button> 
-    </form>
-  )
+class PostAddForm extends Component {
+
+  state = { 
+    text: ''
+  }
+
+  onValueChanged = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+
+    this.props.onAdd(this.state.text)
+    this.setState({
+      text: ''
+    })
+    
+  }
+
+  render() {
+    return (
+      <form 
+        className="bottom-panel d-flex"
+        onSubmit={this.onSubmit}>
+        <input 
+          type="text"
+          placeholder="Input your note here"
+          className="form-control new-post-label"
+          onChange={ this.onValueChanged }
+          value={ this.state.text }
+        />
+        <button type="submit"
+                className="btn btn-outline-secondary"
+                >
+          Add note
+        </button> 
+      </form>
+    )
+  }
+  
 }
 
 export default PostAddForm
